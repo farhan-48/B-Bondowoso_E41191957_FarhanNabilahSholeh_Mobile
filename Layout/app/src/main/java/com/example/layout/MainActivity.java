11ea23b78dfa831d245d963d6733da8c1373941a
farhan-48 <71968283+farhan-48@users.noreply.github.com>
2021-03-29 15:44:24 +0700

    Minggu ke-3

---
+++ Layout/app/src/main/java/com/example/layout/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Pada, Subject, Pesan;
+    Button Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
